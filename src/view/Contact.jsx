52121bfd95b953f8963315064fc44ed1b55710cf
Yogesh Sharma <yogeshsharma132000@gmail.com>
2022-03-10 04:58:27 +0000

    Added Contact Form

--- src/view/Contact.jsx
+++ src/view/Contact.jsx
@@ -1,10 +1,52 @@
 import React from 'react';
 
 function Contact() {
+	const [formData, setFormData] = React.useState({
+		name:"",
+		email:"",
+		message:""
+	})
+	
+	const handleChange = function (event) {
+		const {name, value} = event.target
+		setFormData(prevData => (
+			{
+				...prevData,
+				[name]:value
+			}
+		))
+	}
+	const handleSubmit = function (event) {
+		event.preventDefault()
+		console.log(formData)
+	}
   return (
     <div>
-			<h1> Hello Contact </h1>
-			
+			<h1> Something to say to me ? </h1>
+			<form className="form" onSubmit={handleSubmit}>
+				<input 
+					type="text" 
+					placeholder="Name" 
+					name ="name"
+					onChange={handleChange}
+					value={formData.name}
+				/>
+				<input 
+					type="email" 
+					placeholder="abc@xyz.com"
+					name="email"
+					onChange={handleChange}
+					value={formData.email}
+				/>
+				<textarea 
+					placeholder="Message for me" 
+					name="message"
+					value={formData.message}
+					onChange={handleChange}
+				/>
+				<button >Send</button>
+				
+			</form>
 		</div>
   );
 }
